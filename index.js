a9bb49cca021f8cc28f95c457ec1832b77b41b3b
JAYANTJOSHI001 <jayantjoshi909@gmail.com>
2025-10-31 16:27:30 +0530

    setup node
    Signed-off-by: JAYANTJOSHI001 <jayantjoshi909@gmail.com>

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
